d17ead90c917206ceebce8acf6fb758bce691afa
praise <praise@windows.com>
2026-03-13 19:08:53 +0100

    fix API URLs for production

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-       target: "http://localhost:8000",  // ← your backend URL
+       target: "https://api.journita.ai",  // ← your backend URL
         changeOrigin: true,
         secure: true,
       },
